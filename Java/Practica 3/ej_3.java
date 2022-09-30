71c94e6431cb9ce7bef12ddb77d867038d5130b4
nadienuncanada <109698373+nadienuncanada@users.noreply.github.com>
2022-09-30 16:26:14 -0300

    Update ej_3.java

--- Java/Practica 3/ej_3.java
+++ Java/Practica 3/ej_3.java
@@ -40,3 +40,48 @@ public class Estantes {
     }
     
 }
+//class2
+package tema3;
+
+
+public class Estante {
+    private int dimL;
+    private final int dimF = 20;
+    private Libro[] vector;
+//-------------------constructor-----------
+    
+    public Estante() {
+        this.dimL = 0;
+        this.vector=new Libro[dimF];
+
+    }
+//------------------getters----------------
+    public Libro[] getVector() {
+        return vector;
+    }
+//-----------------setters-----------------
+    public void setVector(Libro[] vector) {
+        this.vector = vector;
+    }
+//----------------metodos------------------
+    public void agregar_libro(Libro lib){
+        this.vector[dimL]=lib;
+        this.dimL++;
+    }
+    
+    public int cant_libros(){
+        return this.dimL;
+    }
+    public boolean lleno(){
+        return (dimL==20);
+    }
+    public Libro buscar_titulo(String titulo){
+        for (int i = 0; i < this.dimL; i++) {
+            if (this.vector[i].getTitulo().equalsIgnoreCase(titulo)) {
+                return this.vector[i];
+            }
+            
+        }
+        return null;
+    }
+}
