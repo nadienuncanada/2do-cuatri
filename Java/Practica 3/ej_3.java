3dc906af9507916471cb1c5a7f63f80642304183
nadienuncanada <109698373+nadienuncanada@users.noreply.github.com>
2022-09-30 17:23:56 -0300

    Update ej_3.java

--- Java/Practica 3/ej_3.java
+++ Java/Practica 3/ej_3.java
@@ -1,3 +1,45 @@
+package tema3;
+
+import PaqueteLectura.Lector;
+
+/**
+ *
+ * @author alumnos
+ */
+public class ej_03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Estantes nuevo = new Estantes();
+        nuevo.setEstantes(datoLibro());
+        System.out.println(nuevo.getLibro("Mujercitas"));
+
+        
+    }
+    public static Libro datoLibro(){
+    System.out.println("Ingresar Nombre de Autor: ");
+        String nom= Lector.leerString();
+    System.out.println("Ingresar Biografia: ");
+        String bio= Lector.leerString();
+    System.out.println("Ingresar Origen de Autor: ");
+        String ori= Lector.leerString();    
+    AutorN aut= new AutorN(nom,bio,ori);
+        System.out.println("Nombre del Libro: ");
+            String nl=Lector.leerString();
+        System.out.println("Ingresar Editorial: ");
+            String edi=Lector.leerString();
+        System.out.println("Ingresar anio de Edicion: ");
+            int anio=Lector.leerInt();
+        System.out.println("Ingresar ISBN: ");
+            String isbn=Lector.leerString();
+        System.out.println("Ingresar Precio: ");
+            double pre=Lector.leerDouble();
+    return new Libro(nl,edi,anio,aut,isbn,pre);
+}
+    
+}
 //class
 package tema3;
 
