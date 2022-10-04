f4f86a904394fe7eba5496a6d0e48ab3b5dae816
nadienuncanada <109698373+nadienuncanada@users.noreply.github.com>
2022-10-04 16:45:55 -0300

    Update ej_01,a.java

--- Java/Practica 4/ej_01,a.java
+++ Java/Practica 4/ej_01,a.java
@@ -72,3 +72,57 @@ setLado3(c);
     }
 
 }
+    //class 2
+    package tema4;
+
+/**
+ *
+ * @author alumnos
+ */
+public abstract class Empleado {
+    private String nombre;
+    private double sueldo;
+    private int antiguedad;
+
+    public Empleado(String nombre, double sueldo, int antiguedad) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.antiguedad = antiguedad;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    public abstract double calcularEfectividad ();
+    public abstract double calcularSueldoACobrar ();
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "nombre=" + nombre + ", sueldo=" + sueldo + ", antiguedad=" + antiguedad + '}';
+    }
+    
+    
+    
+    
+}
